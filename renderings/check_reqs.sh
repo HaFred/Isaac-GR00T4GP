a2280d8201bcdafe4725e0e05bf6744a5657a552
Frederick Hong <haurunis@outlook.com>
2025-05-02 21:25:26 +0800

    update dependencies

--- renderings/check_reqs.sh
+++ renderings/check_reqs.sh
@@ -1,3 +1,4 @@
-pip install -U ffmpeg
-pip install mediapy
+conda install -c conda-forge ffmpeg=4.2.2
+pip install -U mujoco
+pip install -U mediapy
 pip install gymnasium==0.29.1, gymnasium-robotics==1.2.4
